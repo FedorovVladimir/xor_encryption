c9bab5f570f984d409cb6ade315060bc630ec0ca
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 16:02:57 +0700

    add cli

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+import click
+
+
+@click.command()
+@click.option("-i", "input_file", required=True, help="Путь до файла для обработки")
+@click.option("-o", "output_file", required=True, help="Путь до выходного файла")
+@click.option("-k", "key", required=True, help="Ключ шифрования")
+def process(input_file: str, output_file: str, key: str):
+    pass
+
+
+if __name__ == "__main__":
+    process()
