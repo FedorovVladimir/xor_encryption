2d3baa8b78b25d90f883ad9109da64af87fa9c41
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 17:54:58 +0700

    add work

--- work.py
+++ work.py
@@ -1,2 +1,10 @@
+from encryption import encryption
+
+COUNT_CHARS_FOR_READ = 1024 * 1024
+
+
 def work(input_file_path: str, output_file_path: str, key: str):
-    pass
+    with open(input_file_path, 'rb') as input_file, open(output_file_path, 'wb') as output_file:
+        input_text = input_file.read(COUNT_CHARS_FOR_READ)
+        output_text = encryption(input_text, key)
+        output_file.write(output_text)
