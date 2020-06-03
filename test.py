87d6dd3bf77810ee27352722af8049d7d23153d1
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 16:20:10 +0700

    add rename test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from encryption import encryption
 
 class XorTest(unittest.TestCase):
 
-    def test_xor(self):
+    def test_xor_1(self):
         input_text = 'hello world'
         key = 'key'
         self.assertNotEqual(input_text, encryption(input_text, key))
