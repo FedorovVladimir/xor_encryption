f22417d7d5665d955482d5ae9cc51d1b84162e8f
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 16:27:34 +0700

    add test 2

--- encryption.py
+++ encryption.py
@@ -1,2 +1,7 @@
 def encryption(input_text: str, key: str):
-    pass
+    output_text = ''
+    for i in range(len(input_text)):
+        code_input_text_char = ord(input_text[i])
+        code_key_char = ord(key[i % len(key)])
+        output_text += chr(code_input_text_char ^ code_key_char)
+    return output_text
--- test.py
+++ test.py
@@ -9,3 +9,10 @@ class XorTest(unittest.TestCase):
         input_text = 'hello world'
         key = 'key'
         self.assertNotEqual(input_text, encryption(input_text, key))
+
+    def test_xor_2(self):
+        input_text = 'hello world'
+        key = 'key'
+        output_text = encryption(input_text, key)
+        output_text = encryption(output_text, key)
+        self.assertEqual(input_text, output_text)
