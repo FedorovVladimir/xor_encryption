d850018120891a728190279d44ad49b28c8cd066
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 19:40:59 +0700

    add threads

--- work.py
+++ work.py
@@ -1,10 +1,19 @@
-from encryption import encryption
+from MyThread import MyThread
 
 COUNT_CHARS_FOR_READ = 1024 * 1024
 
 
 def work(input_file_path: str, output_file_path: str, key: str):
     with open(input_file_path, 'rb') as input_file, open(output_file_path, 'wb') as output_file:
+        threads = []
+
         input_text = input_file.read(COUNT_CHARS_FOR_READ)
-        output_text = encryption(input_text, key)
-        output_file.write(output_text)
+        while input_text != b'':
+            thread = MyThread(input_text, key)
+            threads.append(thread)
+            thread.start()
+            input_text = input_file.read(COUNT_CHARS_FOR_READ)
+
+        for thread in threads:
+            thread.join()
+            output_file.write(thread.output_text)
