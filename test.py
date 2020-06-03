e975f2f0a5bf83f52258cced10dd66d545d01d0b
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 16:30:01 +0700

    add test 3

--- test.py
+++ test.py
@@ -16,3 +16,13 @@ class XorTest(unittest.TestCase):
         output_text = encryption(input_text, key)
         output_text = encryption(output_text, key)
         self.assertEqual(input_text, output_text)
+
+    def test_xor_3(self):
+        input_text = 'hello world'
+        good_key = 'good key'
+        bad_key = 'bad key'
+        output_text = encryption(input_text, good_key)
+        good_output_text = encryption(output_text, good_key)
+        bad_output_text = encryption(output_text, bad_key)
+        self.assertEqual(input_text, good_output_text)
+        self.assertNotEqual(input_text, bad_output_text)
