30dd946884a70ee4755bc814a012b8c991efedfd
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 17:36:25 +0700

    change work with bytes

--- encryption.py
+++ encryption.py
@@ -1,7 +1,7 @@
-def encryption(input_text: str, key: str):
-    output_text = ''
+def encryption(input_text: [], key: str):
+    output_text = b''
     for i in range(len(input_text)):
-        code_input_text_char = ord(input_text[i])
-        code_key_char = ord(key[i % len(key)])
-        output_text += chr(code_input_text_char ^ code_key_char)
+        code_input_text_byte = input_text[i]
+        code_key_byte = ord(key[i % len(key)])
+        output_text += (code_input_text_byte ^ code_key_byte).to_bytes(1, byteorder='little')
     return output_text
--- test.py
+++ test.py
@@ -6,19 +6,19 @@ from encryption import encryption
 class XorTest(unittest.TestCase):
 
     def test_xor_1(self):
-        input_text = 'hello world'
+        input_text = b'hello world'
         key = 'key'
         self.assertNotEqual(input_text, encryption(input_text, key))
 
     def test_xor_2(self):
-        input_text = 'hello world'
+        input_text = b'hello world'
         key = 'key'
         output_text = encryption(input_text, key)
         output_text = encryption(output_text, key)
         self.assertEqual(input_text, output_text)
 
     def test_xor_3(self):
-        input_text = 'hello world'
+        input_text = b'hello world'
         good_key = 'good key'
         bad_key = 'bad key'
         output_text = encryption(input_text, good_key)
